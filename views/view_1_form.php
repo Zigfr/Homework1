d065f8c9757adf42f757406c705511b05d779268
Zigfr <metropoliten17@mail.ru>
2017-01-23 23:00:57 +0300

    This view first form

---
+++ views/view_1_form.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+    <html>
+    <head>
+        <title> News creation </title>
+    </head>
+    <body>
+        <h2> Please insert your news in this form </h2>
+        <form action = 'controller_1.php' method = 'POST'>
+            <input type = 'textarea' name = 'info'>
+            <input type = 'submit'>
+        
+        </form>
+        
+    </body>
+    
+    </html>

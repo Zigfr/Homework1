10a357144b96d490443b16ea7999aedc66f1af7f
Zigfr <metropoliten17@mail.ru>
2017-01-24 12:56:03 +0300

    New limit for 10 strings

--- model_2.php
+++ model_2.php
@@ -2,5 +2,5 @@
 include __DIR__ .'/autoload.php';
 
 $dat = new DB();
-$sql2 ='SELECT * FROM `mainnew` ORDER BY `time_now` DESC';
+$sql2 ='SELECT * FROM `mainnew` ORDER BY `time_now` DESC LIMIT 10';
 $sth = $dat->sellect($sql2);
